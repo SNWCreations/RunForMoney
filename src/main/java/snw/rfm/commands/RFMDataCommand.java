a69ba40c247cbed6829ab9d16bd2e67a7f87538b
ZX夏夜之风 <snwcreations@qq.com>
2022-05-04 18:12:40 +0800

    First implementation of /rfmdata!

---
+++ src/main/java/snw/rfm/commands/RFMDataCommand.java
@@ -0,0 +1,166 @@
+/*
+ * This file is part of RunForMoney.
+ *
+ * RunForMoney is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * RunForMoney is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with RunForMoney. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package snw.rfm.commands;
+
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CommandPermission;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import snw.rfm.ItemRegistry;
+import snw.rfm.RunForMoney;
+import snw.rfm.commands.admin.RFMTimerCommand;
+import snw.rfm.config.GameConfiguration;
+import snw.rfm.config.Preset;
+import snw.rfm.game.GameProcess;
+import snw.rfm.game.TeamHolder;
+import snw.rfm.processor.EventProcessor;
+import snw.rfm.util.LanguageSupport;
+import snw.rfm.util.NickSupport;
+import snw.rfm.util.PlaceHolderString;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+
+import static snw.rfm.Util.sortDescend;
+import static snw.rfm.commands.admin.ExportListCommand.SDF;
+
+public class RFMDataCommand {
+    public static void register() {
+        new CommandAPICommand("rfmdata")
+                // if no subcommand specified, this statement will be executed.
+                // TODO complete this
+                .executes((sender, args) -> {
+                    sender.sendMessage(ChatColor.GOLD + "--- RFMGroup help ---");
+                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rfmdata.help.coin"));
+                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rfmdata.help.exportcoin"));
+                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rfmdata.help.playerremaining"));
+                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rfmdata.help.timer"));
+                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rfmdata.help.settings"));
+                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rfmdata.help.reload"));
+                })
+                .withSubcommand(
+                        new CommandAPICommand("coin") // equals /coinlist
+                                .executes((sender, args) -> {
+                                    Map<String, Double> coinEarned = sortDescend(RunForMoney.getInstance().getCoinEarned());
+                                    if (coinEarned.size() == 0) {
+                                        sender.sendMessage(ChatColor.RED + LanguageSupport.getTranslation("commands.coinlist.empty"));
+                                    } else {
+                                        sender.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + LanguageSupport.getTranslation("commands.coinlist.header"));
+                                        int a = 0;
+                                        for (Map.Entry<String, Double> keypair : coinEarned.entrySet()) {
+                                            sender.sendMessage(ChatColor.GREEN + "" + ++a + ". " + keypair.getKey() + ": " + keypair.getValue());
+                                        }
+                                    }
+                                })
+                )
+                .withSubcommand(
+                        new CommandAPICommand("exportcoin") // equals /exportcoinlist
+                                .withPermission(CommandPermission.OP) // only op can do this
+                                .executes((sender, args) -> {
+                                    RunForMoney rfm = RunForMoney.getInstance();
+                                    Map<String, Double> coinEarned = rfm.getCoinEarned();
+                                    if (coinEarned.size() == 0) {
+                                        sender.sendMessage(ChatColor.RED + LanguageSupport.replacePlaceHolder("$commands.operation_failed$ $commands.coinlist.empty$"));
+                                    } else {
+                                        String date = SDF.format(new Date()); // TODO after the command re-write
+                                                                              // move SDF to this class
+                                        String fileName = rfm.getDataFolder().getAbsolutePath() + File.separator + date + ".txt";
+                                        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) { // 2022/2/7 改用 try-with-resource 结构
+                                            // region 写入头
+                                            writer.write(LanguageSupport.getTranslation("commands.coinlist.header"));
+                                            writer.newLine();
+                                            writer.write(LanguageSupport.getTranslation("commands.exportlist.created_time") + date);
+                                            writer.newLine();
+                                            writer.newLine();
+                                            // endregion
+                                            int a = 0;
+                                            for (Map.Entry<String, Double> e : coinEarned.entrySet()) {
+                                                writer.write(++a + "." + e.getKey() + ": " + e.getValue()); // 写入排号，玩家名，B币数量
+                                                writer.newLine(); // 换行，否则数据会成一大坨。。
+                                            }
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                            throw CommandAPI.fail(LanguageSupport.replacePlaceHolder("$commands.operation_failed$ $commands.exportlist.unable_to_export$"));
+                                        }
+                                        sender.sendMessage(ChatColor.GREEN + LanguageSupport.replacePlaceHolder("$commands.operation_success$ $commands.exportlist.unable_to_export$"));
+                                    }
+                                })
+                )
+                .withSubcommand(
+                        new CommandAPICommand("playerremaining") // equals /playerremaining
+                                .executes((sender, args) -> {
+                                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.prc.runner_header") + String.join(", ", TeamHolder.getInstance().getRunners()));
+                                    String nigText = String.join(",", TeamHolder.getInstance().getOutPlayers());
+                                    Set<String> giveUp = TeamHolder.getInstance().getGiveUpPlayers();
+                                    if (!giveUp.isEmpty()) {
+                                        nigText = nigText + ", " + String.join(", ", giveUp);
+                                    }
+                                    sender.sendMessage(ChatColor.RED + LanguageSupport.getTranslation("commands.prc.not_in_game_header") + nigText);
+                                })
+                )
+                .withSubcommand(
+                        new CommandAPICommand("timer") // equals /rfmtimer
+                                .executesPlayer((sender, args) -> {  // only player can do this
+                                        if (RFMTimerCommand.getSeePlayers().contains(sender.getName())) {
+                                            RFMTimerCommand.getSeePlayers().remove(sender.getName());
+                                            sender.sendMessage(ChatColor.RED + LanguageSupport.getTranslation("commands.rfmtimer.disabled"));
+                                        } else {
+                                            RFMTimerCommand.getSeePlayers().add(sender.getName());
+                                            sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rfmtimer.enabled"));
+                                        }
+                                })
+                )
+                .withSubcommand(
+                        new CommandAPICommand("settings") // equals /rsq
+                                .executes((sender, args) -> {
+                                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rsq.hunter_release_time_default") + GameConfiguration.getReleaseTime());
+                                    Location erl = GameConfiguration.getEndRoomLocation();
+                                    //noinspection ConstantConditions
+                                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rsq.endroom_location") +
+                                            (erl != null ?
+                                                    new PlaceHolderString(LanguageSupport.getTranslation("commands.rsq.endroom_part2"))
+                                                            .replaceArgument("x", erl.getBlockX())
+                                                            .replaceArgument("y", erl.getBlockY())
+                                                            .replaceArgument("z", erl.getBlockZ())
+                                                            .replaceArgument("worldName", erl.getWorld().getName())
+                                                            .toString()
+                                                    : LanguageSupport.getTranslation("commands.rsq.not_set_yet")));
+                                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rsq.game_time") + GameConfiguration.getGameTime());
+                                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rsq.coin_per_second") + GameConfiguration.getCoinPerSecond());
+                                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.rsq.multiplier") + GameConfiguration.getCoinMultiplierOnBeCatched());
+                                })
+                )
+                .withSubcommand(
+                        new CommandAPICommand("reload") // equals /rfmreload
+                                .withPermission(CommandPermission.OP)
+                                .executes((sender, args) -> {
+                                    RunForMoney.getInstance().reloadConfig();
+                                    LanguageSupport.loadLanguage(RunForMoney.getInstance().getConfig().getString("language", "zh_CN"));
+                                    GameConfiguration.init();
+                                    Preset.init();
+                                    NickSupport.init();
+                                    ItemRegistry.unregisterItem("ep");
+                                    ItemRegistry.unregisterItem("hpc");
+                                    RunForMoney.getInstance().registerInternalItems();
+                                    EventProcessor.init();
+                                    GameProcess.init();
+                                    sender.sendMessage(ChatColor.GREEN + LanguageSupport.getTranslation("commands.operation_success"));
+                                })
+                )
+                .register();
+    }
+}
